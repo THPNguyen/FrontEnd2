565cd38e54c577717ca19d79a5315de3a230160b
THPNguyen <18211tt4361@gmail.com>
2022-06-18 22:15:17 +0700

    INIT PROJECT VERSON   : 1 CONG NGHE:  LARAVEL+REACTJS AUTHERS  : TRUNG HAU & TRONG TINH DATE     : 6/18/2022

---
+++ front-end/src/pages/Home.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import http from "../http";
+
+const Home = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchAllUsers();
+  }, []);
+  
+  const fetchAllUsers = () => {
+    http.get("/users").then((res) => {
+      setUsers(res.data);
+    });
+  };
+  const deleteUser = (id) => {
+    http.delete("/users/" + id).then((res) => {
+      fetchAllUsers();
+    });
+  };
+  return (
+    <div>
+      <h2>Users listing ....</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Sno.</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Acion</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={user.id}>
+              <td>{++index}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>
+                <Link
+                  className="btn btn-warning"
+                  to={{ pathname: "/edit/" + user.id }}
+                >
+                  Edit
+                </Link>
+                <Link
+                  className="btn btn-primary"
+                  to={{ pathname: "/view/" + user.id }}
+                >
+                  View
+                </Link>
+                <button
+                  className="btn btn-danger"
+                  onClick={()=>{deleteUser(user.id)}}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Home;
---
+++ front-end/src/pages/View.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import http from "../http";
+const Edit = (props) => {
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({});
+  const [users, setUsers] =useState([]);
+  const {id} = useParams();
+
+  useEffect(() =>{
+    fetchUser()
+  }, [])
+  const fetchUser = () =>{
+    http.get('/users/' +id+'/edit').then((res)=> {
+      setInputs({
+        name:res.data.name,
+        email:res.data.email,
+      });
+    })
+  }
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const submitForm = (event) => {
+    http.put("/users/"+id, inputs).then((res) => {
+      navigate("/");
+    });
+  };
+  return (
+    <div>
+      <h2>Edit User</h2>
+      <div className="row">
+        <div className="col-sm-6">
+          <div className="card p-4">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="form-control mb-2"
+              value={inputs.name || ""}
+              onChange={handleChange}
+            />
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="form-control mb-2"
+              value={inputs.email || ""}
+              onChange={handleChange}
+            />
+            <button
+              type="button"
+              onClick={submitForm}
+              className={"btn btn-primary"}
+            >
+              Update
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Edit;
