77738655d0a4b52b7acbb9aad93bf0f0fdef393d
THPNguyen <18211tt4361@gmail.com>
2022-06-21 21:48:37 +0700

    update file cart, login, register

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,85 +2,90 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Link, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./pages/Users/Home";
+import Home from "./pages/Home";
 import Create from "./pages/Users/Create";
 import Edit from "./pages/Users/Edit";
 import View from "./pages/Users/View";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import { Slide } from "@mui/material";
 function App() {
   return (
-    <div>
-      <nav className="navbar navbar-expand-sm navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
-        <button
-          className="navbar-toggler d-lg-none"
-          type="button"
-          data-toggle="collapse"
-          data-target="#collapsibleNavId"
-          aria-controls="collapsibleNavId"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="collapsibleNavId">
-          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li className="nav-item active">
-              <Link to={"/"} className={"nav-link"}>
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/create"} className={"nav-link"}>
-                Create
-              </Link>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="dropdownId"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <div className="dropdown-menu" aria-labelledby="dropdownId">
-                <a className="dropdown-item" href="#">
-                  Action 1
-                </a>
-                <a className="dropdown-item" href="#">
-                  Action 2
-                </a>
-              </div>
-            </li>
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input
-              className="form-control mr-sm-2"
-              type="text"
-              placeholder="Search"
-            />
-            <button
-              className="btn btn-outline-success my-2 my-sm-0"
-              type="submit"
-            >
-              Search
-            </button>
-          </form>
-        </div>
-      </nav>
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/create" element={<Create />}></Route>
-          <Route path="/edit/:id" element={<Edit />}></Route>
-          <Route path="/view/:id" element={<View />}></Route>
-        </Routes>
-      </div>
-    </div>
+    // <div>
+    //   <nav className="navbar navbar-expand-sm navbar-light bg-light">
+    //     <a className="navbar-brand" href="#">
+    //       Navbar
+    //     </a>
+    //     <button
+    //       className="navbar-toggler d-lg-none"
+    //       type="button"
+    //       data-toggle="collapse"
+    //       data-target="#collapsibleNavId"
+    //       aria-controls="collapsibleNavId"
+    //       aria-expanded="false"
+    //       aria-label="Toggle navigation"
+    //     >
+    //       <span className="navbar-toggler-icon"></span>
+    //     </button>
+    //     <div className="collapse navbar-collapse" id="collapsibleNavId">
+    //       <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+    //         <li className="nav-item active">
+    //           <Link to={"/"} className={"nav-link"}>
+    //             Home
+    //           </Link>
+    //         </li>
+    //         <li className="nav-item">
+    //           <Link to={"/create"} className={"nav-link"}>
+    //             Create
+    //           </Link>
+    //         </li>
+    //         <li className="nav-item dropdown">
+    //           <a
+    //             className="nav-link dropdown-toggle"
+    //             href="#"
+    //             id="dropdownId"
+    //             data-toggle="dropdown"
+    //             aria-haspopup="true"
+    //             aria-expanded="false"
+    //           >
+    //             Dropdown
+    //           </a>
+    //           <div className="dropdown-menu" aria-labelledby="dropdownId">
+    //             <a className="dropdown-item" href="#">
+    //               Action 1
+    //             </a>
+    //             <a className="dropdown-item" href="#">
+    //               Action 2
+    //             </a>
+    //           </div>
+    //         </li>
+    //       </ul>
+    //       <form className="form-inline my-2 my-lg-0">
+    //         <input
+    //           className="form-control mr-sm-2"
+    //           type="text"
+    //           placeholder="Search"
+    //         />
+    //         <button
+    //           className="btn btn-outline-success my-2 my-sm-0"
+    //           type="submit"
+    //         >
+    //           Search
+    //         </button>
+    //       </form>
+    //     </div>
+    //   </nav>
+    //   <div className="container">
+    //     <Routes>
+    //       <Route path="/" element={<Home />}></Route>
+    //       <Route path="/create" element={<Create />}></Route>
+    //       <Route path="/edit/:id" element={<Edit />}></Route>
+    //       <Route path="/view/:id" element={<View />}></Route>
+    //     </Routes>
+    //   </div>
+    // </div>
+    <Cart />
   );
 }
 
